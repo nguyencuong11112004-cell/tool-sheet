639b4863e5820452036e5ad17c760f34fe113847
unknown <chicuongnguyen193@gmail.com>
2026-07-02 22:50:40 +0700

    bla

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const RECOVERY_DOMAINS = [
 ];
 
 const OTP_POLL_INTERVAL_MS = 5000;
-const DEFAULT_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx4F8oP-85av3budf-LUiNdBo_0nr957Jv8LO-9UJ9Sta1gV4j4t6P0fgb713bd2deTtg/exec';
+const DEFAULT_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyCH_4_6XtbeR1_VRXopO5Dposnv8t8EuieXGnBKB_GC-MfVvulfCWv0RCPJYDVi4jsuA/exec';
 
 const HEADER_ALIASES = {
   name: ['ten', 'name'],
